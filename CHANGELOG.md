e954ddec78326f122684b1fb370d6bb985d3722e
Brian Mock <brian@mockbrian.com>
2018-12-29 15:43:11 -0800

    Many changes :)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+# v1.1.0
+
+- Adds `--color` flag
+- Adds `--no-color` flag
+- Adds `--no-progress` flag
+- Adds `--add-babel-plugin <plugin>` flag
+- Improves parse error formatting
+- Adds suggestions on which Babel plugins to add
+
 # v1.0.1
 
 - Updates package.json to point at the GitHub repo
--- src/cli.js
+++ src/cli.js
@@ -3,18 +3,34 @@ const program = require("commander");
 const pkg = require("../package.json");
 const cmd = require("./cmd").default;
 
+function concat(x, acc) {
+  acc.push(x);
+  return acc;
+}
+
 program.name(pkg.name);
 program.version(pkg.version, "-v, --version");
 
 // TODO:
-// - Flag for --quiet to hide progress output
 // - Flag for --sort <alphabetical|usage>
 // - Flag for --report <all|...> to specify which data to report
-// - Flag for --color <yes|no|auto>
 
 program
   .arguments("[components...]")
   .description("Displays a report of JSX component and prop usage")
+  .option("--no-progress", "do not show progress messages")
+  // NOTE: chalk automatically reads process.argv for these flags, and commander
+  // does not correctly handle allowing both "--foo" and "--no-foo" flags
+  //
+  // https://github.com/tj/commander.js/issues/108
+  .option("--color", "force enable color")
+  .option("--no-color", "force disable color")
+  .option(
+    "--add-babel-plugin <plugin>",
+    "add a babel plugin (can be repeated)",
+    concat,
+    []
+  )
   .option("--no-gitignore", "disable reading .gitignore files")
   .option(
     "--directory <directory>",
@@ -27,6 +43,7 @@ program
   );
 
 program.on("--help", () => {
+  // eslint-disable-next-line no-console
   console.log(`
 Examples:
   # Display info for every component
@@ -35,13 +52,19 @@ Examples:
   # Display info only for <div> and <Tab.Container>
   $ ${pkg.name} div Tab.Container
 
+
+  # Enable Babel plugins
+  $ ${
+    pkg.name
+  } --add-babel-plugin decorators-legacy --add-babel-plugin pipelineOperator
+
 Documentation can be found at https://github.com/wavebeem/jsx-info
 `);
 });
 
 function main() {
   program.parse(process.argv);
-  cmd(program.args, program);
+  cmd();
 }
 
 exports.default = main;
