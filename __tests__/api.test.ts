db9f2df94424c7bafc03caae3530f87a28babeeb
Brian Mock <brian@wavebeem.com>
2020-12-17 15:33:24 -0800

    fix it for travis

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,68 +1,60 @@
 import * as path from "path";
-import { analyze } from "../src/api";
+import { Analysis, analyze } from "../src/api";
+
+function clean(analysis: Analysis, dir: string) {
+  const { filenames, elapsedTime, ...data } = analysis;
+  return {
+    ...data,
+    elapsedTime: elapsedTime * 0,
+    filenames: filenames.map((f) => f.replace(dir, "$ROOT")),
+  };
+}
 
 test("analyze empty directory", async () => {
-  const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/empty"),
-  });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  const directory = path.resolve(__dirname, "../fixtures/empty");
+  const analysis = await analyze({ directory });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
 
 test("analyze basic directory", async () => {
-  const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/basic"),
-  });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  const directory = path.resolve(__dirname, "../fixtures/basic");
+  const analysis = await analyze({ directory });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
 
 test("analyze ts directory", async () => {
-  const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/ts"),
-  });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  const directory = path.resolve(__dirname, "../fixtures/ts");
+  const analysis = await analyze({ directory });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
 
 test("analyze error directory", async () => {
-  const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/error"),
-  });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  const directory = path.resolve(__dirname, "../fixtures/error");
+  const analysis = await analyze({ directory });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
 
 test("analyze decorators directory", async () => {
-  const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/decorators"),
-  });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  const directory = path.resolve(__dirname, "../fixtures/decorators");
+  const analysis = await analyze({ directory });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
 
 test("analyze decorators directory with plugin decorators-legacy", async () => {
+  const directory = path.resolve(__dirname, "../fixtures/decorators");
   const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/decorators"),
+    directory,
     babelPlugins: ["decorators-legacy"],
   });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
 
 test("analyze prop-usage directory", async () => {
+  const directory = path.resolve(__dirname, "../fixtures/prop-usage");
   const analysis = await analyze({
-    directory: path.resolve(__dirname, "../fixtures/prop-usage"),
+    directory,
     components: ["div"],
     prop: "a",
   });
-  expect(analysis).toMatchSnapshot({
-    elapsedTime: expect.any(Number),
-  });
+  expect(clean(analysis, directory)).toMatchSnapshot();
 });
